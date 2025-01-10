a66726548ca4a26e7cb7dae93becb5472cf7f3f0
Felix Niemeyer <niemeyer.felix@gmail.com>
2025-01-10 11:00:04 +0100

    update glomap process to use one camera for each folder of images

--- sfm/process_images_glomap.sh
+++ sfm/process_images_glomap.sh
@@ -7,12 +7,18 @@ if [ -d "$project_folder" ]; then
     exit 1
 fi
 
+# 90 degree => 0.5 focal length factor 
+flf=0.5
+
+
 mkdir -p $project_folder
 
 echo 'extracting features'
 time colmap feature_extractor \
     --database_path $project_folder/database.db \
     --image_path $image_folder \
+    --single_camera_per_folder 1 \
+	--ImageReader.default_focal_length_factor $flf \ 
     > $project_folder/feature_extractor.log 2>&1
 
 echo 'matching features'
@@ -29,11 +35,62 @@ time glomap mapper \
     --output_path $project_folder/sparse \
     > $project_folder/glomap.log 2>&1
 
-echo 'undistorting'
-time colmap image_undistorter \
-    --image_path $image_folder \
-    --input_path $project_folder/sparse/0 \
-    --output_path $project_folder/undistorted \
-    --output_type COLMAP \
-    > $project_folder/image_undistorter.log 2>&1
 
+# optimization
+
+sparse_folder="$project_folder/sparse/0"
+
+echo 'bundle adjustment'
+bundle_adjusted_dir="$project_folder/bundle_adjusted"
+mkdir -p "$bundle_adjusted_dir"
+time colmap bundle_adjuster \
+	--input_path $sparse_folder \
+	--output_path $bundle_adjusted_dir \
+	2>&1 | tee $project_folder/bundle_adjuster.log 
+# paused args
+#	--BundleAdjustment.refine_focal_length 1 \
+#	--BundleAdjustment.refine_principal_point 1 \
+#	--BundleAdjustment.refine_extra_params 1 \
+
+image_filtered_dir=$bundle_adjusted_dir #skip
+# 90 degree => 1.4142 / 2
+# flr=0.7071
+# flr_min=$(echo "$flr - 0.05" | bc)
+# flr_max=$(echo "$flr + 0.05" | bc)
+# image_filtered_dir="$project_folder/image_filtered"
+# colmap image_filterer \
+# 	--input_path $bundle_adjusted_dir \
+#     --output_path $image_filtered_dir \
+#     --min_focal_length_ratio $flr_min \
+#     --max_focal_length_ratio $flr_max \
+
+echo 'filtering'
+filtered_dir="$project_folder/filtered"
+mkdir -p "$filtered_dir"
+time colmap point_filtering \
+	--input_path $bundle_adjusted_dir \
+	--output_path $filtered_dir \
+	--min_track_len 3 \
+	--max_reproj_error 3.0 \
+	--min_tri_angle 2.0 \
+	2>&1 | tee $project_folder/point_filtering.log 
+
+# estimate orientation
+echo 'estimating orientation'
+oriented_dir="$project_folder/oriented"
+mkdir -p "$oriented_dir"
+time colmap model_orientation_aligner \
+	--image_path $image_folder \
+    --input_path $filtered_dir \
+    --output_path $oriented_dir \
+    2>&1 | tee $project_folder/orientation.log
+
+echo 'undistorting'
+undistorted_dir="$project_folder/undistorted"
+mkdir -p "$undistorted_dir"
+time colmap image_undistorter \
+	--image_path $image_folder \
+	--input_path $oriented_dir \
+	--output_path $undistorted_dir \
+	--output_type COLMAP \
+	2>&1 | tee $project_folder/image_undistorter.log 
