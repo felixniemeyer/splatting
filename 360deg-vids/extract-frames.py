027a938c259069ee8e13063df2f338a192615943
Felix Niemeyer <niemeyer.felix@gmail.com>
2025-01-10 10:59:32 +0100

    option to keep perspectives in separate folders => different cams

--- 360deg-vids/extract-frames.py
+++ 360deg-vids/extract-frames.py
@@ -13,6 +13,7 @@ parser.add_argument("--rate", type=float, default=0.4, help="frame rate")
 parser.add_argument("--fov", type=int, default=90, help="field of view")
 parser.add_argument("--cam_man", type=int, default=95, help="degree at which the camera man is")
 parser.add_argument("--pitch", type=int, default=0, help="pitch")
+parser.add_argument("--separate_folders", action="store_true")
 
 args = parser.parse_args()
 
@@ -23,7 +24,7 @@ class Perspective:
         self.roll = roll
 
     def filename(self): 
-        return f"p{self.pitch}_y{self.yaw}"
+        return f"p{self.pitch}y{self.yaw}"
 
     def __str__(self): 
         return f"pitch: {self.pitch}, yaw: {self.yaw}"
@@ -56,10 +57,10 @@ class Circle:
         self.cam_man_segment = cam_man_segment
 
 circles = [
-    Circle(0, args.fov * 0.5, 90),
-    Circle(35, args.fov * 0.8, 60),
-    Circle(-35, args.fov * 0.8, 160), # nach unten 
-    Circle(70, args.fov * 1.1, 60),
+    Circle(10, args.fov * 0.5, 90),
+    Circle(55, args.fov * 0.7, 60),
+    Circle(-35, args.fov * 0.5, 200), # nach unten 
+#    Circle(90, args.fov * 1.1, 60),
 ]
 
 for circle in circles:
@@ -67,8 +68,8 @@ for circle in circles:
     for iyaw in range(num):
         perspectives.append(Perspective(circle.pitch, yaw_start + iyaw * deg_step, 0))
 
-command = "mkdir -p " + args.outdir + " \n"
-command += f'ffmpeg -i "{args.input}" -filter_complex '
+mkfolders = "mkdir -p " + args.outdir + " \n"
+command = f'ffmpeg -i "{args.input}" -filter_complex '
 
 filter_complex = "\"\n"
 outputs = ""
@@ -76,12 +77,18 @@ outputs = ""
 for i, perspective in enumerate(perspectives): 
     perspective.clamp_yaw()
     filter_complex += f"[0:v]v360=input=equirect:output=rectilinear:h_fov={args.fov}:v_fov={args.fov}:pitch={perspective.pitch}:yaw={perspective.yaw}:roll={perspective.roll}:w={args.res}:h={args.res}[v{i}];\n"
-    # make folder
-
-    outputs += f"-map \"[v{i}]\" -r {args.rate} \"{args.outdir}/%05d_{i:03d}_{perspective.filename()}.jpg\" \\\n"
+    if args.separate_folders: 
+        # make folder
+        folder = f"{args.outdir}/{perspective.filename()}"
+        mkfolders += f"mkdir -p {folder} \n"
+        outputs += f"-map \"[v{i}]\" -r {args.rate} \"{folder}/%05d.jpg\" \\\n"
+    else:
+        outputs += f"-map \"[v{i}]\" -r {args.rate} \"{args.outdir}/%05d_{i:03d}_{perspective.filename()}.jpg\" \\\n"
 
 filter_complex += "\" \\\n"
 
 command += filter_complex + outputs
 
+print(mkfolders)
 print(command)
+
